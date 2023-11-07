3cf575b93a4686c81f6160d140f6f88f77a7160b
Leyla-Mammad <leylamamedova19960@gmail.com>
2023-11-08 00:27:58 +0400

    getset

---
+++ getset1/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Linq;
+
+public class Student
+{
+    private int[] grades;
+
+    public int[] Grades
+    {
+        get { return grades; }
+        set { grades = value.Where(x => x % 2 == 0).ToArray(); }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student st = new Student();
+        st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+        // Printing the filtered grades
+        Console.WriteLine(string.Join(", ", st.Grades)); // Output: 2, 4, 6, 8
+    }
+}
